773296b4c909d622e19275f55a16c2faada93ac0
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-06 16:17:30 -0400

    make get request using async-await within useEffect, access retrieved data to display in component using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
+import { useState, useEffect } from 'react';
 import { clientId, authToken } from './config.tsx';
 
-async function getTopClips(clientId: string, authToken: string) {
-  try {
-    const response = await fetch("https://api.twitch.tv/helix/clips?game_id=509658&started_at=2024-10-03T16:36:58Z&ended_at=2024-10-04T16:36:58Z&is_featured=true", {
-      method: 'GET',
-      headers: {
-        'Client-Id': clientId,
-        'Authorization': 'Bearer ' + authToken
+export default function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getTopClips(clientId: string, authToken: string) {
+        try {
+          const response = await fetch("https://api.twitch.tv/helix/clips?game_id=509658&started_at=2024-10-03T16:36:58Z&ended_at=2024-10-04T16:36:58Z&is_featured=true", {
+            method: 'GET',
+            headers: {
+              'Client-Id': clientId,
+              'Authorization': 'Bearer ' + authToken
+            }
+          });
+          const clipsData = await response.json();
+          console.log(clipsData.data[0].id);
+          setData(clipsData.data[0].id)
+          return clipsData; // or do something with it
+        } catch (error) {
+          console.error(error);
+        }
       }
-    });
-    const data = await response.json();
-    console.log(data); // Return data here
-    return data; // or do something with it
-  } catch (error) {
-    console.error(error);
-  }
-}
 
-function App() {
-  getTopClips(clientId, authToken)
+    getTopClips(clientId, authToken);
+    return () => {}
+  }, []);
+
   return (
-    <h1>HELLO</h1>
-  )
+    <>
+        <p>First id</p>
+        <p>{data}</p>  
+    </>
+  );
 }
-
-export default App
